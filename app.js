d8b94448d44258fcc95f1d539cb4e12bddab16f8
whitePeter1 <90694516+whitePeter1@users.noreply.github.com>
2021-09-21 12:58:48 +0200

    Following Tutorial
    Following Tutorial

--- app.js
+++ app.js
@@ -11,13 +11,22 @@ const database = require('./db')
 const mainRoute = require('./routes/main')
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.post('/login1', (req,res)=>{
+    const jwt = require("jsonwebtoken")
+const accessToken = (req,res)=>{
+    const username = req.body.Username 
+    const user = {name:username}
+const accessToken  = jwt.sign(user,process.env.secretkey,)
+    res.json({accessToken:accessToken})
+}
+}) // Pending Tutorial https://www.youtube.com/watch?v=mbsmsi7l3r4
 // Register view engnie
 app.set('view engine', 'ejs')
 // Routes Post Middleware
-
 app.use('/', mainRoute)
 app.use('/products', productsRoute)
 app.use('/user', require('./routes/user'))
 app.use('/views',express.static(path.join(__dirname,"public")))
 app.listen(3000);
+
 
