3c8b3bc23a10ecb6824dbacd3c466fd83e1a312c
whitePeter1 <90694516+whitePeter1@users.noreply.github.com>
2021-09-29 15:28:26 +0200

    MAIN
    MAIN

--- controller/helpers/tokenauthenticate.js
+++ controller/helpers/tokenauthenticate.js
@@ -4,10 +4,10 @@ const jwt = require('jsonwebtoken')
 const authenticateToken = async (req,res,next)=>
 {
   let token = req.cookies.token
-jwt.verify(token,process.env.secretkey,(err,user1)=>{
+jwt.verify(token,process.env.secretkey,(err,test123)=>{
     if(err) return res.status(403).redirect('/loginregister');
     process.env.loggedin = "true"
-    console.log(user1)
+    console.log("You have been logged in as " + test123)
     console.log(process.env.loggedin)
     next();
 })
