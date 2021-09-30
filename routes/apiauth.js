a36155e484af87ab92104548e5c1665083ab6890
whitePeter1 <90694516+whitePeter1@users.noreply.github.com>
2021-09-30 14:08:45 +0200

    MAIN
    MAIN

--- routes/apiauth.js
+++ routes/apiauth.js
@@ -5,6 +5,18 @@ const jwt = require('jsonwebtoken')
 const apiToken = require('../models/apiAuth')
 const {authenticateapi} = require('../controller/helpers/apiauthenticate')
 const {generateToken} = require('../controller/generateToken')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+router.get('/api123',(req,res)=>{
+    fetch("https://pensure-dev.azurewebsites.net/api/Partner/SalesSupport/Employer/Init",{
+        "Content-Type": "application/json",
+        "method": "GET"
+    })
+    .then(data =>{
+        res.send(data)
+    })
+    .catch(err=>{console.log(err)})
+})
 router.get('/protected',authenticateapi,(req,res)=>{
     const test = req.query.api2
 console.log(test)
