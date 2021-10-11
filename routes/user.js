3b9ed9c8cb119d2740a1f328ec9a0d0d54380b00
whitePeter1 <90694516+whitePeter1@users.noreply.github.com>
2021-10-11 16:53:10 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,13 +4,21 @@ const path = require('path')
 const product = require('../models/user')
 const userController = require('../controller/user')
 const {allUsers, allUsersPost,allUsersDelete,allUsersUpdate} = require('../controller/allusersjson')
+const headeroptions = (req,res,next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            res.header('Access-Control-Allow-Headers', 'Content-Type');
+            next();
+}
 router.get('/',userController.userspage)
 router.post('/',userController.adduser)
 router.post('/search',userController.SearchRes)
 router.get('/del/:DeleteParam',userController.Delete)
-router.get('/json',allUsers)
-router.post('/json',allUsersPost)
-router.delete('/json',allUsersDelete)
-router.put('/json',allUsersUpdate)
+router.get('/json',headeroptions,allUsers)
+router.post('/json',headeroptions,allUsersPost)
+router.delete('/json',headeroptions,allUsersDelete)
+router.put('/json',headeroptions,allUsersUpdate)
 
 module.exports = router;
+
+
