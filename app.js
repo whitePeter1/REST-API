8026c0e6121c0fc87b0f69584d5a8af6f7d20cea
whitePeter1 <90694516+whitePeter1@users.noreply.github.com>
2021-10-11 11:27:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/register',(req,res)=>{
     res.render('register')
 })
 app.all('/'), function(req,res,next){
-    res.header('Access-Control-Allow-Origin', 'nodejsapi443.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
